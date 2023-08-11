e1f5cf32e059d529e76bdc34fc4a7ad67eea67b6
Riyansh Garg <riyanshgarg2103@gmail.com>
2023-08-11 16:19:37 +0530

    Added All files of Project

---
+++ Variables.js
@@ -0,0 +1,13 @@
+    //Get Elements
+    const firebaseConfig = {
+        apiKey: "AIzaSyDVBAYcTlRY6RQ5ICKSp6Av7vNa9k5JRjY",
+        authDomain: "richpanel-b0372.firebaseapp.com",
+        projectId: "richpanel-b0372",
+        storageBucket: "richpanel-b0372.appspot.com",
+        messagingSenderId: "1075105340077",
+        appId: "1:1075105340077:web:d236ae6a2a625ed70d8424",
+        measurementId: "G-2F108HP4ZB"
+      };
+    
+      // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
